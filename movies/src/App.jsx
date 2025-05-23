43f4408e244e5a3a77baa23fc92413d971ecbdf1
Santiago Cuervo <cuervo.santiago@gmail.com>
2025-05-23 17:05:32 -0500

    Render navbar in App.jsx

--- movies/src/App.jsx
+++ movies/src/App.jsx
@@ -2,15 +2,19 @@ import './App.css';
 import { Favorite } from './pages/Favorites';
 import { Home } from './pages/Home';
 import { Routes, Route } from 'react-router-dom';
+import { NavBar } from './components/NavBar';
 
 function App() {
   return (
-    <main className="main-content">
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/favorites" element={<Favorite />}></Route>
-      </Routes>
-    </main>
+    <div>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/favorites" element={<Favorite />}></Route>
+        </Routes>
+      </main>
+    </div>
   );
 }
 
