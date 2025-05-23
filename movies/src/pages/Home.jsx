5c7ccc5a8db356cd93953b9f718a68f576696d70
Santiago Cuervo <cuervo.santiago@gmail.com>
2025-05-23 18:26:21 -0500

    Add useEffect() only render popular movies when the component is loaded for the first time

--- movies/src/pages/Home.jsx
+++ movies/src/pages/Home.jsx
@@ -1,15 +1,28 @@
 import '../css/Home.css';
 import { MovieCard } from '../components/MovieCard';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { getPopularMovies, searchMovies } from '../services/api';
 
 export function Home() {
   const [searchQuery, setSearchQuery] = useState('');
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-  const movies = [
-    { id: 1, title: 'John Wick', release_date: '2020' },
-    { id: 2, title: 'Terminator', release_date: '1999' },
-    { id: 3, title: 'The Matrix', release_date: '1998' },
-  ];
+  useEffect(() => {
+    const loadPopularMovies = async () => {
+      try {
+        const popularMovies = await getPopularMovies();
+        setMovies(popularMovies);
+      } catch (err) {
+        setError('Failed to load movies...');
+        console.log(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    loadPopularMovies();
+  }, []);
 
   const handleSearch = (e) => {
     e.preventDefault();
