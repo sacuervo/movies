9d0bfd7ea963d1860f3f5dc78d01e90a528c5b19
Santiago Cuervo <cuervo.santiago@gmail.com>
2025-05-23 15:28:53 -0500

    Add search box and button

--- movies/src/pages/Home.jsx
+++ movies/src/pages/Home.jsx
@@ -7,8 +7,20 @@ export function Home() {
     { id: 3, title: 'The Matrix', release_date: '1998' },
   ];
 
+  const handleSearch = () => {};
+
   return (
     <div className="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="Search for movies..."
+          className="search-input"
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
       <div className="movies-grid">
         {movies.map((movie) => (
           <MovieCard movie={movie} key={movie.id} />
