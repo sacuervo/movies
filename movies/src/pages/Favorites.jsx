fbcc1ba8f772aa455e236a1180dc2777c5926e45
Santiago Cuervo <cuervo.santiago@gmail.com>
2025-05-23 16:50:31 -0500

    Add favorites component

---
+++ movies/src/pages/Favorites.jsx
@@ -0,0 +1,8 @@
+export function Favorite() {
+  return (
+    <div className="favorites-empty">
+      <h2>No Favorite Movies Yet</h2>
+      <p>Start adding movies to your favorites and they will appear here.</p>
+    </div>
+  );
+}
