938463428042a0df108fea5cbaee9d6e62ba8ab4
Santiago Cuervo <cuervo.santiago@gmail.com>
2025-05-23 16:58:55 -0500

    Add component routing to the App.jsx

--- movies/src/App.jsx
+++ movies/src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css';
+import { Favorite } from './pages/Favorites';
 import { Home } from './pages/Home';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <main className="main-content">
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/favorites" element={<Favorite />}></Route>
+      </Routes>
+    </main>
   );
 }
 
