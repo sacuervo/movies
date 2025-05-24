7fab115f440ab263a1032bdb88ff383c3aa28e04
Santiago Cuervo <cuervo.santiago@gmail.com>
2025-05-23 19:35:41 -0500

    Add movie context

--- movies/src/App.jsx
+++ movies/src/App.jsx
@@ -3,10 +3,11 @@ import { Favorite } from './pages/Favorites';
 import { Home } from './pages/Home';
 import { Routes, Route } from 'react-router-dom';
 import { NavBar } from './components/NavBar';
+import { MovieProvider } from './contexts/MovieContext';
 
 function App() {
   return (
-    <div>
+    <MovieProvider>
       <NavBar />
       <main className="main-content">
         <Routes>
@@ -14,7 +15,7 @@ function App() {
           <Route path="/favorites" element={<Favorite />}></Route>
         </Routes>
       </main>
-    </div>
+    </MovieProvider>
   );
 }
 
