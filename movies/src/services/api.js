7f85b0916663b362d5a265a891fafaf441d41976
Santiago Cuervo <cuervo.santiago@gmail.com>
2025-05-23 20:30:58 -0500

    Do API calls with Axios

--- movies/src/services/api.js
+++ movies/src/services/api.js
@@ -1,18 +1,20 @@
+import axios from 'axios';
+
 const API_KEY = '92fcf522cd30d3bce3a19123cbf08c32';
 const BASE_URL = 'https://api.themoviedb.org/3';
 
 export const getPopularMovies = async () => {
-  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
-  const data = await response.json();
-  return data.results;
+  const response = await axios.get(
+    `${BASE_URL}/movie/popular?api_key=${API_KEY}`
+  );
+  return response.data.results;
 };
 
 export const searchMovies = async (query) => {
-  const response = await fetch(
+  const response = await axios.get(
     `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
       query
     )}`
   );
-  const data = await response.json();
-  return data.results;
+  return response.data.results;
 };
